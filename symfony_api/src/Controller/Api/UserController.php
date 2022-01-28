7026f8bb4c30ee7c101602f33da8450a55ec6dd5
JHGDev <jhgdeveloper@gmail.com>
2022-01-28 02:43:24 +0100

    Agregada funcionalidad para crear el usuario Sector labora - un solo valorl Conocimientos - varios valores posibles

--- symfony_api/src/Controller/Api/UserController.php
+++ symfony_api/src/Controller/Api/UserController.php
@@ -1,7 +1,13 @@
 <?php
 namespace App\Controller\Api;
 
+use App\Entity\Knowledge;
+use App\Entity\KnowledgeAssignments;
+use App\Entity\LaboralSector;
+use App\Entity\LaboralSectorAssignments;
 use App\Entity\User;
+use App\Repository\KnowledgeRepository;
+use App\Repository\LaboralSectorRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
@@ -34,12 +40,14 @@ class UserController extends AbstractFOSRestController
      * @Rest\Post(path="/users")
      * @Rest\View(serializerGroups={"user"},serializerEnableMaxDepthChecks=true)
      */
-    public function postAction(Request $request, EntityManagerInterface $em)
+    public function postAction(Request $request, EntityManagerInterface $em, LaboralSectorRepository $laboralSectorRepository, KnowledgeRepository $knowledge_repository)
     {
         $email = $request->get('email', null);
         $firstname = $request->get('firstname', null);
         $lastname = $request->get('lastname', null);
         $birth_date_timestamp = $request->get('birth_date', null);
+        $laboral_sector = $request->get('laboral_sector', null);
+        $knowledge = $request->get('knowledge', null);
 
         $error_messages = $this->checkDataInputCreate($email, $firstname, $lastname, $birth_date_timestamp);
 
@@ -55,8 +63,62 @@ class UserController extends AbstractFOSRestController
              ->setFirstname($firstname)
              ->setLastname($lastname)      
              ->setBirthDate($birth_date);
-
+        
         $em->persist($user);
+
+        if (!empty($laboral_sector)){
+            
+            $query = ['name' => $laboral_sector];
+            if ($resultado = $laboralSectorRepository->findOneBy($query)){
+                $laboralSectorAssignment = new LaboralSectorAssignments();
+                $laboralSectorAssignment->setUserId($user)
+                                        ->setLaboralSectorId($resultado);
+                
+                $em->persist($laboralSectorAssignment);
+            }else{
+                $laboral_sector_new = new LaboralSector();
+                $laboral_sector_new->setName($laboral_sector);
+
+                $em->persist($laboral_sector_new);
+
+                $laboralSectorAssignment = new LaboralSectorAssignments();
+                $laboralSectorAssignment->setUserId($user)
+                                        ->setLaboralSectorId($laboral_sector_new);
+                
+                $em->persist($laboralSectorAssignment);
+            }
+        }
+
+
+        if (!empty($knowledge)){
+            
+            $knowledge_array = explode(',', $knowledge);
+
+            foreach($knowledge_array as $user_knowledge_name){
+             
+                $query = ['name' => $user_knowledge_name];
+                if ($resultado = $knowledge_repository->findOneBy($query)){
+                    $knowledgeAssignment = new KnowledgeAssignments();
+                    $knowledgeAssignment->setUserId($user)
+                                            ->setKnowledgeId($resultado);
+                    
+                    $em->persist($knowledgeAssignment);
+                }else{
+                    $knowledge_new = new Knowledge();
+                    $knowledge_new->setName($user_knowledge_name);
+
+                    $em->persist($knowledge_new);
+
+                    $knowledgeAssignment = new KnowledgeAssignments();
+                    $knowledgeAssignment->setUserId($user)
+                                        ->setKnowledgeId($knowledge_new);
+                    
+                    $em->persist($knowledgeAssignment);
+                }
+            }
+        }
+
+
         $em->flush();
 
         $this->logger->info('User created');
